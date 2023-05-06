8915b15656f1344e661897f253f42740d7c89b71
Billy Torres <btorresr75@gmail.com>
2023-05-06 16:40:37 -0600

    daySixUnfinished

---
+++ daySeven/index.js
@@ -0,0 +1,16 @@
+const searchBtn = document.querySelector(".nav__search-btn")
+const labelSerach = document.querySelector(".nav__search")
+const menuBtn = document.querySelector(".nav__menu-btn")
+const navTitle = document.querySelector(".nav__title")
+
+searchBtn.addEventListener("click", () => {
+    if(labelSerach.style.display === "none") {
+        labelSerach.style.display = "block"
+        menuBtn.style.display = "none"
+        navTitle.style.display = "none"
+    } else {
+        labelSerach.style.display = "none"
+        menuBtn.style.display = "block"
+        navTitle.style.display = "block"
+    }
+})
