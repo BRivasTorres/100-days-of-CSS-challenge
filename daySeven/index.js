44b643c25b1f4561fab98d7327b1ce5ed74d7ee8
Billy Torres <btorresr75@gmail.com>
2023-05-13 17:46:57 -0600

    daySeven

--- daySeven/index.js
+++ daySeven/index.js
@@ -2,8 +2,10 @@ const searchBtn = document.querySelector(".nav__search-btn")
 const labelSerach = document.querySelector(".nav__search")
 const menuBtn = document.querySelector(".nav__menu-btn")
 const navTitle = document.querySelector(".nav__title")
-
-const menuNavBtn = document.querySelector(".nav__menu-btn")
+const navMenuBtn = document.querySelector(".nav__menu-btn")
+const navMenu = document.querySelector(".nav__menu")
+const nav = document.querySelector(".nav")
+const hero = document.querySelector(".hero")
 
 searchBtn.addEventListener("click", () => {
     if(labelSerach.style.display === "none") {
@@ -17,6 +19,16 @@ searchBtn.addEventListener("click", () => {
     }
 })
 
-menuNavBtn.addEventListener("click", () => {
-      
+navMenuBtn.addEventListener("click", () => {
+    if(navMenu.style.display === "none") {
+        navMenu.style.display = "block"
+        navMenu.style.left = "0%"
+        nav.style.left = "50%"
+        hero.style.left = "50%"
+    } else {
+        navMenu.style.display = "none"
+        navMenu.style.left = "5%"
+        nav.style.left = "0%"
+        hero.style.left = "0%"
+    }    
 })
