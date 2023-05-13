d390d009cda28295b2611e2b1c333e8f1537ef5c
Billy Torres <btorresr75@gmail.com>
2023-05-13 16:32:13 -0600

    changes

--- daySeven/index.js
+++ daySeven/index.js
@@ -3,6 +3,8 @@ const labelSerach = document.querySelector(".nav__search")
 const menuBtn = document.querySelector(".nav__menu-btn")
 const navTitle = document.querySelector(".nav__title")
 
+const menuNavBtn = document.querySelector(".nav__menu-btn")
+
 searchBtn.addEventListener("click", () => {
     if(labelSerach.style.display === "none") {
         labelSerach.style.display = "block"
@@ -13,4 +15,8 @@ searchBtn.addEventListener("click", () => {
         menuBtn.style.display = "block"
         navTitle.style.display = "block"
     }
+})
+
+menuNavBtn.addEventListener("click", () => {
+      
 })
